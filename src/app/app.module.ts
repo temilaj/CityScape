bb8eeaa46d166085435e9740c197284d472d694a
Temi Lajumoke <temi@temilajumoke.com>
2017-08-20 14:22:20 +0100

    integrate firebase

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,9 +7,26 @@ import { AboutPage } from '../pages/about/about';
 import { ContactPage } from '../pages/contact/contact';
 import { HomePage } from '../pages/home/home';
 import { TabsPage } from '../pages/tabs/tabs';
+import { LoginPage } from "../pages/login/login";
 
 import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
+
+import { AngularFireModule } from 'angularfire2';
+import { AngularFireDatabaseModule } from 'angularfire2/database';
+import { AngularFireAuthModule } from 'angularfire2/auth';
+
+import { environment } from "../environments/environment";
+
+const { apiKey, authDomain, databaseURL, storageBucket, messagingSenderId } = environment;
+
+export const firebaseConfig = {
+  apiKey,
+  authDomain,
+  databaseURL,
+  storageBucket,
+  messagingSenderId,
+};
 
 @NgModule({
   declarations: [
@@ -17,11 +34,15 @@ import { SplashScreen } from '@ionic-native/splash-screen';
     AboutPage,
     ContactPage,
     HomePage,
-    TabsPage
+    TabsPage,
+    LoginPage
   ],
   imports: [
     BrowserModule,
-    IonicModule.forRoot(MyApp)
+    IonicModule.forRoot(MyApp),
+    AngularFireModule.initializeApp(firebaseConfig),
+    AngularFireDatabaseModule,
+    AngularFireAuthModule
   ],
   bootstrap: [IonicApp],
   entryComponents: [
---
+++ src/environments/environment.sample.ts
@@ -0,0 +1,7 @@
+export const environment = {
+  apiKey: 'API_KEY_HERE',
+  authDomain: 'API_KEY_HERE',
+  databaseURL: 'API_KEY_HERE',
+  storageBucket: 'API_KEY_HERE',
+  messagingSenderId: 'API_KEY_HERE'
+};

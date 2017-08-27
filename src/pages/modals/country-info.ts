ded2a40272e48436470177250ba23979c8b16d4c
Temi Lajumoke <temi@temilajumoke.com>
2017-08-27 23:55:57 +0100

    add country info modal

---
+++ src/pages/modals/country-info.ts
@@ -0,0 +1,21 @@
+import { Component } from '@angular/core';
+
+import { ViewController, NavController, NavParams, ToastController } from 'ionic-angular';
+
+
+@Component({
+  templateUrl: 'country-info.html',
+})
+export class CountryInfoPage {
+  country;
+
+  constructor(public viewCtrl: ViewController, 
+    public navCtrl: NavController,  public navParams: NavParams) {
+      this.country = this.navParams.get('country');
+  }
+
+  dismiss() {
+    this.viewCtrl.dismiss();
+  }
+
+}
